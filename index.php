c21315d2cd82dec8ba75f6821cc9676543feeb31
John Chris Cayetano <cayetanojohnchris@gmail.com>
2023-12-04 21:21:21 +0800

    Base

---
+++ index.php
@@ -0,0 +1,200 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Alorica | Welcome</title>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="assets/css/font-awesome.min.css" rel="stylesheet" />
+    <link href="assets/css/prettyPhoto.css" rel="stylesheet" />
+    <link href="assets/css/style.css" rel="stylesheet" />
+</head>
+
+<body>
+    <header id="header-nav">
+        <div id="navbar" class="navbar" style="background-color: white;">
+            <div class="navbar-header">
+                <img src="assets/img/logo.svg" width="160px" alt="Alorica">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+            </div>
+            <div class="collapse navbar-collapse">
+                <ul class="nav navbar-nav flot-nav">
+                    <li><a href="#slide-head"><i class="fa fa-home color-red"></i> Home</a></li>
+                    <li><a href="#about-section"><i class="fa fa-question color-brown"></i> About Us</a></li>
+                    <li><a href="#services-section"><i class="fa fa-check color-green"></i> Services</a></li>
+                </ul>
+            </div>
+        </div>
+    </header>
+
+    <section id="slide-head" class="carousel" style="background-color: white; width: 100%;">
+        <div class="carousel-inner">
+            <div class="item active"
+                style="background-image: url('assets/img/bg-1.jpg'); background-repeat: no-repeat; background-size: cover;">
+                <div class="container">
+                    <div class="carousel-content">
+                        <h1>Transforming the Customer Experience</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="item"
+                style="background-image: url('assets/img/bg-2.jpg'); background-repeat: no-repeat; background-size: cover;">
+                <div class="container">
+                    <div class="carousel-content">
+                        <h1>Inspired by People, Empowered by Technology</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <section id="about-section">
+        <div class="wrap-pad">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1 ">
+                    <div class="text-center">
+                        <h1><i class="fa fa-question small-icons bk-color-brown"></i>Alorica</h1>
+                        <p class="lead">
+                            Alorica is one of the largest, certified minority-owned BPO is the digital customer
+                            experience partner of choice for innovative companies all over the world.
+                            We are Alorica—100,000+ strong and dedicated to creating insanely great experiences for
+                            customers everywhere. It is our great privilege to represent the best brands in the world,
+                            and we embrace every day and every challenge with passion, performance, and possibilities.
+
+                            Some see the sky as the limit. We see it as a stepping stone.
+                        </p>
+                    </div>
+
+                </div>
+
+                <div class="col-md-10 col-md-offset-1 col-sm-10 col-sm-offset-1 margin-top-100  ">
+                    <div class="col-md-3 col-sm-6" data-scrollreveal="enter left and move 100px, wait 0.6s">
+                        <div class="text-center">
+                            <p>
+                                <img class="img-responsive img-thumbnail img-circle" src="assets/img/founder.png"
+                                    alt="">
+                            </p>
+                            <h3>Andy Lee</h3>
+                            <p>
+                                Alorica said that Haller has led the company through historic growth, key client wins
+                                and success in the deployment of its digital solutions including the exponential
+                                expansion of its work-at-home solution, Alorica Anywhere.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+    <!--END ABOUT SECTION -->
+    <!--SERVICES SECTION -->
+    <section id="services-section">
+        <div class="wrap-pad">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1 "
+                    data-scrollreveal="enter top and move 100px, wait 0.5s">
+                    <div class="text-center">
+                        <h1><i class="fa fa-check small-icons bk-color-green"></i>Services</h1>
+                        <p class="lead">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec enim sapien. Aliquam erat
+                            volutpat.
+                            Quisque eu ante at tortor imperdiet gravida nec sed turpis. Phasellus augue augue.
+                        </p>
+                    </div>
+                </div>
+                <!-- ./ Heading div-->
+                <div class="col-md-10 col-md-offset-1 col-sm-10 col-sm-offset-1 ">
+                    <div class="col-md-4 col-sm-6" data-scrollreveal="enter left and move 100px, wait 0.6s">
+                        <div class="text-center">
+                            <a href="https://www.alorica.com/industries/gaming">
+                                <i class="fa fa-gamepad icon-round bk-color-red"></i>
+                            </a>
+                            <h4>Gaming</h4>
+                            <p>
+                                Do not play games with your game play support—you need scalable, effective services to
+                                ensure the best player experience.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-6" data-scrollreveal="enter bottom and move 100px, wait 0.6s">
+                        <div class="text-center">
+                            <a href="https://www.alorica.com/industries/online-community">
+                                <i class="fa fa-comments icon-round bk-color-black"></i>
+                            </a>
+                            <h4>Community</h4>
+                            <p>
+                                Your brand, your fans, your community.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-6" data-scrollreveal="enter right and move 100px, wait 0.6s">
+                        <div class="text-center">
+                            <a href="https://www.alorica.com/industries/technology">
+                                <i class="fa fa-desktop icon-round bk-color-light-blue"></i>
+                            </a>
+                            <h4>Tech</h4>
+                            <p>
+                                With over 20 years on the digital forefront, we know a thing or two about connecting
+                                customers with technology.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-6" data-scrollreveal="enter left and move 100px, wait 0.6s">
+                        <div class="text-center">
+                            <a href="https://www.alorica.com/industries/health-and-lifestyle">
+                                <i class="fa fa-flask icon-round bk-color-green"></i>
+                            </a>
+                            <h4>Health and Lifestyle</h4>
+                            <p>
+                                Grow your business and patient loyalty by exceeding CX and digital engagement
+                                expectations.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-6" data-scrollreveal="enter bottom and move 100px, wait 0.6s">
+                        <div class="text-center">
+                            <a href="https://www.alorica.com/industries/public-sector">
+                                <i class="fa fa-pencil icon-round bk-color-blue"></i>
+                            </a>
+                            <h4>Public Sector</h4>
+                            <p>
+                                Service for the citizens you serve in SLED (State, Local, and Education)
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-6" data-scrollreveal="enter right and move 100px, wait 0.6s">
+                        <div class="text-center">
+                            <a href="https://www.alorica.com/industries/subscription-services">
+                                <i class="fa fa-thumbs-up icon-round bk-color-brown"></i>
+                            </a>
+                            <h4>Subscription</h4>
+                            <p>
+                                Keep calm and stream on.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <script src="assets/js/jquery.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/jquery.prettyPhoto.js"></script>
+    <script src="assets/js/scrollReveal.js"></script>
+    <script src="assets/scripts/custom.js"></script>
+</body>
+
+</html>
